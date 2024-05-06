478fd94f3655acf5fe00845cd3ff3a278aa3d3c8
Thejellyfish1024 <alamin102410@gmail.com>
2024-05-06 23:19:30 +0600

    made some changes

--- app/about/me/page.js
+++ app/about/me/page.js
@@ -5,6 +5,9 @@ const Me = () => {
         <div>
             I am Rahim. facing a problem 
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, ut!
+            <div>
+                I am 23 years old. I like problem solving.
+            </div>
         </div>
     );
 };
