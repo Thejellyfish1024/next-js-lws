711c96aeb12a73714bae760bf1cd743a4d741468
Thejellyfish1024 <alamin102410@gmail.com>
2024-05-06 21:26:09 +0600

    made some changes

--- app/about/me/page.js
+++ app/about/me/page.js
@@ -4,6 +4,7 @@ const Me = () => {
     return (
         <div>
             I am Rahim. facing a problem 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, ut!
         </div>
     );
 };
